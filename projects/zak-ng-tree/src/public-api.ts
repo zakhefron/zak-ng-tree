95fb032dbd535a754d8e59823af89021908320fe
zakhefron <zakhefron@gmail.com>
2021-10-31 23:33:39 +0530

    removed fontawesome

--- projects/zak-ng-tree/src/public-api.ts
+++ projects/zak-ng-tree/src/public-api.ts
@@ -3,5 +3,5 @@
  */
 
 // export * from './lib/zak-ng-tree.service';
-// export * from './lib/zak-ng-tree.component';
+export * from './lib/zak-ng-tree.component';
 export * from './lib/zak-ng-tree.module';
